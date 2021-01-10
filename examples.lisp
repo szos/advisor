1fe6404fd78acb3705a5711f24123cf31008960e
Nathan Shostek <szos@posteo.net>
2021-01-10 06:42:44 +0100

    modified adder examples

--- examples.lisp
+++ examples.lisp
@@ -8,10 +8,22 @@
 (in-package #:advisor-examples)
 
 (defun adder (a b &rest numbers)
+  "add a, b and all other provided numbers"
   (apply '+ (cons a (cons b numbers))))
 
 (defadvice :before adder (&rest ignore)
+  "Advice to run before adder"
   (format t "~&calling adder like so:  (adder ~{~A~^ ~})~%" ignore))
+
+(defadvice :around adder (&rest numbers)
+  "advice to wrap around adder function"
+  (declare (ignorable numbers))
+  (format t "~&wrapping adder~%")
+  (call-main-function))
+
+(defadvice :after adder (&rest numbers)
+  "Advice to run after adder"
+  (format t "~&called adder like so:  (adder ~{~A~^ ~})" numbers))
 
 (defun keytest (&key (print-me 'HI))
   (format nil "~A" print-me))
